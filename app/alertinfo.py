56df7c6cc36d1de0bb432602fcd89bbd0e2b4d8b
LeeZJ <lizhejie1996@gmail.com>
2024-04-15 18:53:54 +0800

    增加按时间,告警名沉默告警功能

--- app/alertinfo.py
+++ app/alertinfo.py
@@ -5,65 +5,47 @@
 Let zeal join!
 Copyright (c) 2023 by lizhejie, All Rights Reserved.
 """
+import time
+
 import requests
 import json
 from utils import *
 from setting import *
 
 
-def alert(status, instance, name, severity, system, startat, summary):
-    params = json.dumps({
-        "msgtype": "markdown",
-        "markdown":
-            {
-                "content": "## <font color=\"red\">告警通知: {0}</font>\n**告警实例:** <font color=\"red\">{1}</font>\n**告警名称:** {2}\n**告警级别:** {3}\n**相关系统:** {4}\n**告警时间:** {5}\n**告警详情:** {6}".format(
-                    status, instance, name, severity, system, startat, summary)
-            }
-    })
-
-    return params
-
-
-def recover(status, instance, name, severity, system, startat, endsat, summary):
-    params = json.dumps({
-        "msgtype": "markdown",
-        "markdown":
-            {
-                "content": "## <font color=\"green\">恢复通知: {0}</font>\n**恢复实例:**<font color=\"green\"> {1}</font>\n**告警名称:** {2}\n**告警级别:** {3}\n**相关系统:** {4}\n**告警时间:** {5}\n**恢复时间:** {6}\n**告警详情:** {7}".format(
-                    status, instance, name, severity, system, startat, endsat, summary)
-            }
-    })
-
-    return params
-
-
-def webhook_url(params, url_key):
-    headers = {"Content-type": "application/json"}
-    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={0}".format(url_key)
-    r = requests.post(url, params, headers)
-
-
 def send_alert(json_re, qywxbot_key):
     for alertinfo in json_re['alerts']:
         status = alertinfo.get('status')
-        labels = alertinfo.get('labels')
-        instance = labels.get('instance')
-        # job = labels.get('job')
-        name = labels.get('alertname')
-        severity = labels.get('severity')
-        system = labels.get('system')
-        # team = labels.get('team')
-        annotations = alertinfo.get('annotations')
-        summary = annotations.get('summary')
+        # labels = alertinfo.get('labels')
+        # instance = labels.get('instance')
+        system = alertinfo.get('labels').get('system', 'saas')
+        # namespace = labels.get('namespace')
+        # pod = labels.get('pod') or labels.get('service')
+        alertname = alertinfo.get('labels').get('alertname')
+        # severity = labels.get('severity')
+        # system = labels.get('system')
+        # annotations = alertinfo.get('annotations')
+        # message = annotations.get('message') or annotations.get('description') or annotations.get('summary')
         startat = isotime2localtime(alertinfo.get('startsAt'))
 
-        if status == 'firing' and (not isspecialtime(startat, SILENCE_DICT.get(system, ["Mon:0-1"]))):
-            # endsat = alertinfo.get('endsAt')
-            alert_msg = alert(status, instance, name, severity, system, startat, summary)
+        alertinfo_list = [alertinfo]
+        formatted_alerts = map(format_alert, alertinfo_list)
+        result_string = "\n".join(list(formatted_alerts))
+        # print(result_string)
+
+        if status == 'firing' and (not is_specialtime(startat, SILENCE_DICT.get(system, ALL_SILENCE_TIME))) and (
+                not is_ignorealertname(alertname)):
+            addition_title = "# <font color=\"red\">告警通知:</font>\n**告警时间:** {0}\n".format(startat)
+            alert_msg = alert2(addition_title + result_string)
             webhook_url(alert_msg, qywxbot_key)
-        elif status == 'resolved' and (not isspecialtime(startat, SILENCE_DICT.get(system, ["Mon:0-1"]))):
+            time.sleep(2)
+        elif status == 'resolved' and (not is_specialtime(startat, SILENCE_DICT.get(system, ALL_SILENCE_TIME))) and (
+                not is_ignorealertname(alertname)):
             endsat = isotime2localtime(alertinfo.get('endsAt'))
-            recover_msg = recover(status, instance, name, severity, system, startat, endsat, summary)
+            addition_title = "# <font color=\"green\">恢复通知:</font>\n**告警时间:** {0}\n**恢复时间:** {1}\n".format(
+                startat, endsat)
+            recover_msg = alert2(addition_title + result_string)
             webhook_url(recover_msg, qywxbot_key)
+            time.sleep(2)
         else:
-            pass
+            print("告警命符合规则或告警时间为沉默时间，告警被沉默")
--- app/utils.py
+++ app/utils.py
@@ -6,18 +6,43 @@ Let zeal join!
 Copyright (c) 2023 by lizhejie, All Rights Reserved.
 """
 from datetime import datetime
+import re
+import requests
+import json
+from setting import *
 
 
-# iso时间转换
+# 判断告警名称
+def is_ignorealertname(alertname):
+    if alertname in IGNORE_ALERT_NAME:
+        return True
+    else:
+        return False
+
+
+# iso时间转换，用于转换从alertmanager获取的时间
 def isotime2localtime(isotimestr: str):
-    dt = datetime.fromisoformat(isotimestr.replace("Z", "+00:00"))
+    # 判断最后一位时间是不是2位，2位就补0
+    dot_index = isotimestr.rindex('.')
+    z_index = isotimestr.index('Z')
+    sub_string = isotimestr[dot_index + 1:z_index]
+    numbers = re.findall(r'\d', sub_string)
+    numbers_count = len(numbers)
+    if numbers_count == 2:
+        isotimestr = isotimestr.replace(sub_string, sub_string + "0")
+    else:
+        pass
+    if "Z" in isotimestr:
+        dt = datetime.fromisoformat(isotimestr.replace("Z", "+00:00"))
+    else:
+        dt = datetime.fromisoformat(isotimestr)
     local_dt = dt.astimezone()
     formatted_time = local_dt.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_time
 
 
 # 判断年月日时分秒的时间是否为特定周几几点
-def isspecialtime(formatted_time: str, special_time_list: list):
+def is_specialtime(formatted_time: str, special_time_list: list):
     # 获去告警时间的星期几和小时
     date_format = '%Y-%m-%d %H:%M:%S'
     date = datetime.strptime(formatted_time, date_format)
@@ -41,3 +66,36 @@ def isspecialtime(formatted_time: str, special_time_list: list):
 # 判断某个数是不是在两数之间
 def is_between(number: int, lower_limit: int, upper_limit: int):
     return lower_limit <= number <= upper_limit
+
+
+# 格式化告警信息
+def format_alert(alert):
+    labels = "\n".join([f'<font color="comment">{key}:</font>{value}' for key, value in alert['labels'].items()])
+    annotations = "\n".join(
+        [f'<font color="comment">{key}:</font>{value}' for key, value in alert['annotations'].items()])
+
+    return f"**告警名称: **{alert['labels']['alertname']}\n**告警标签: **\n{labels}\n**告警注解: **\n{annotations}"
+
+
+# 发送企业微信机器人信息
+def webhook_url(params, url_key):
+    headers = {"Content-type": "application/json"}
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={0}".format(url_key)
+    r = requests.post(url, params, headers)
+
+
+# 构建发送请求体
+def alert2(content):
+    params = json.dumps({
+        "msgtype": "markdown",
+        "markdown":
+            {
+                "content": content
+            }
+    })
+
+    return params
+
+
+if __name__ == '__main__':
+    print(is_ignorealertname("TargetDown"))
